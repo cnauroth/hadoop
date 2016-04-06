6db465668a9d131d8e0ac18a1f5e47c99c97841f
Chris Nauroth <cnauroth@apache.org>
2016-04-06 15:36:18 -0700

    HDFS-7240. Provide a bulk read implementation in ChunkInputStream.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/ozone/web/storage/ChunkInputStream.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/ozone/web/storage/ChunkInputStream.java
@@ -80,35 +80,38 @@ class ChunkInputStream extends InputStream {
   public synchronized int read()
       throws IOException {
     checkOpen();
+    int available = prepareRead(1);
+    return available == EOF ? EOF : buffers.get(bufferOffset).get();
+  }
 
-    if (chunks == null || chunks.isEmpty()) {
-      // This must be an empty key.
+  @Override
+  public int read(byte[] b, int off, int len) throws IOException {
+    // According to the JavaDocs for InputStream, it is recommended that
+    // subclasses provide an override of bulk read if possible for performance
+    // reasons.  In addition to performance, we need to do it for correctness
+    // reasons.  The Ozone REST service uses PipedInputStream and
+    // PipedOutputStream to relay HTTP response data between a Jersey thread and
+    // a Netty thread.  It turns out that PipedInputStream/PipedOutputStream
+    // have a subtle dependency (bug?) on the wrapped stream providing separate
+    // implementations of single-byte read and bulk read.  Without this, get key
+    // responses might close the connection before writing all of the bytes
+    // advertised in the Content-Length.
+    if (b == null) {
+      throw new NullPointerException();
+    }
+    if (off < 0 || len < 0 || len > b.length - off) {
+      throw new IndexOutOfBoundsException();
+    }
+    if (len == 0) {
+      return 0;
+    }
+    checkOpen();
+    int available = prepareRead(len);
+    if (available == EOF) {
       return EOF;
     }
-
-    // This loop advances through chunks and buffers as needed until it finds a
-    // byte to return or EOF.
-    for (;;) {
-      if (buffers == null) {
-        // The first read triggers fetching the first chunk.
-        readChunkFromContainer(0);
-      } else if (!buffers.isEmpty() &&
-          buffers.get(bufferOffset).hasRemaining()) {
-        // Data is available from the current buffer.
-        return buffers.get(bufferOffset).get();
-      } else if (!buffers.isEmpty() &&
-          !buffers.get(bufferOffset).hasRemaining() &&
-          bufferOffset < buffers.size() - 1) {
-        // There are additional buffers available.
-        ++bufferOffset;
-      } else if (chunkOffset < chunks.size() - 1) {
-        // There are additional chunks available.
-        readChunkFromContainer(chunkOffset + 1);
-      } else {
-        // All available input has been consumed.
-        return EOF;
-      }
-    }
+    buffers.get(bufferOffset).get(b, off, available);
+    return available;
   }
 
   @Override
@@ -128,6 +131,41 @@ class ChunkInputStream extends InputStream {
   private synchronized void checkOpen() throws IOException {
     if (xceiverClient == null) {
       throw new IOException("ChunkInputStream has been closed.");
+    }
+  }
+
+  /**
+   * Prepares to read by advancing through chunks and buffers as needed until it
+   * finds data to return or encounters EOF.
+   *
+   * @param len desired length of data to read
+   * @return length of data available to read, possibly less than desired length
+   */
+  private synchronized int prepareRead(int len) throws IOException {
+    for (;;) {
+      if (chunks == null || chunks.isEmpty()) {
+        // This must be an empty key.
+        return EOF;
+      } else if (buffers == null) {
+        // The first read triggers fetching the first chunk.
+        readChunkFromContainer(0);
+      } else if (!buffers.isEmpty() &&
+          buffers.get(bufferOffset).hasRemaining()) {
+        // Data is available from the current buffer.
+        ByteBuffer bb = buffers.get(bufferOffset);
+        return len > bb.remaining() ? bb.remaining() : len;
+      } else if (!buffers.isEmpty() &&
+          !buffers.get(bufferOffset).hasRemaining() &&
+          bufferOffset < buffers.size() - 1) {
+        // There are additional buffers available.
+        ++bufferOffset;
+      } else if (chunkOffset < chunks.size() - 1) {
+        // There are additional chunks available.
+        readChunkFromContainer(chunkOffset + 1);
+      } else {
+        // All available input has been consumed.
+        return EOF;
+      }
     }
   }
 
